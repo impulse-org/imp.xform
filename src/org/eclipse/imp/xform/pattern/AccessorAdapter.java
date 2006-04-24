b6f6d648cafff6336eb256662124eae7da91d086
Robert M. Fuhrer <rmfuhrer@optonline.net>
2006-04-24 02:58:47 +0000

    Added interface methods getChildren() and isInstanceOfType(). Added arg to getValue() for the AST node.

--- src/org/eclipse/imp/xform/pattern/AccessorAdapter.java
+++ src/org/eclipse/imp/xform/pattern/AccessorAdapter.java
@@ -6,5 +6,16 @@ import com.ibm.watson.safari.xform.pattern.parser.Ast.NodeAttribute;
  * @author rfuhrer@watson.ibm.com
  */
 public interface AccessorAdapter {
-    public Object getValue(NodeAttribute attribute);
+    public static final String TARGET_TYPE= "targetType";
+
+    public Object getValue(NodeAttribute attribute, Object astNode);
+    public Object getValue(String attributeName, Object astNode);
+
+    /**
+     * @param astNode
+     * @return
+     */
+    public Object[] getChildren(Object astNode);
+
+    public boolean isInstanceOfType(Object astNode, String typeName);
 }
