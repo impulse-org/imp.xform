ce965171a9df80ef10b46355507dc94a49a2417f
Robert M. Fuhrer <rmfuhrer@optonline.net>
2006-05-04 23:30:15 +0000

    Moved test-related classes to separate plugin project.

--- src/org/eclipse/imp/xform/pattern/matching/tests/SystemOutMessageHandler.java
+++
@@ -1,9 +0,0 @@
-package com.ibm.watson.safari.xform.pattern.matching.tests;
-
-import org.eclipse.uide.editor.IMessageHandler;
-
-public class SystemOutMessageHandler implements IMessageHandler {
-    public void handleMessage(int offset, int length, String message) {
-	System.out.println("[" + offset + ":" + length + "] " + message);
-    }
-}
