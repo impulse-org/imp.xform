b548147385d6b897c8a2770e208a9b4ca3ceb0a5
Robert M. Fuhrer <rmfuhrer@optonline.net>
2006-06-22 23:14:10 +0000

    Added default implementation for getValue(), which dispatches the predefined attributes. Added methods for the implementation of the predefined attributes, with some semi-reasonable default behavior. Reworked the various getChild*() methods to make more sense.

--- src/org/eclipse/imp/xform/pattern/parser/ASTAdapterBase.java
+++ src/org/eclipse/imp/xform/pattern/parser/ASTAdapterBase.java
@@ -4,22 +4,92 @@
 package com.ibm.watson.safari.xform.pattern.parser;
 
 import java.lang.reflect.Field;
+import java.lang.reflect.Modifier;
 import java.util.Collections;
 import java.util.Map;
-import java.util.NoSuchElementException;
 import java.util.Set;
+import java.util.Stack;
 
 import com.ibm.watson.safari.xform.pattern.matching.IASTAdapter;
 import com.ibm.watson.safari.xform.pattern.matching.MatchResult;
 import com.ibm.watson.safari.xform.pattern.matching.Matcher;
-import com.ibm.watson.safari.xform.pattern.parser.Ast.NodeAttribute;
 
 public abstract class ASTAdapterBase implements IASTAdapter {
     private static Object[] EMPTY= new Object[0];
 
-    public Object getValue(NodeAttribute attribute, Object astNode) { return null; }
+    /**
+     * Never throws an exception - it is reasonable for a pattern to constrain
+     * via an attribute that not all concrete nodes have.
+     * @return the given attribute of the given ast node, or null if the given
+     * kind of ast node has no such attribute
+     */
+    public final Object getValue(String attributeName, Object astNode) {
+        if (attributeName.equals(KIND))
+            return getKind(astNode);
+        if (attributeName.equals(TARGET_TYPE))
+            return getTargetType(astNode);
+        if (attributeName.equals(NAME))
+            return getName(astNode);
+        return getChildByRole(attributeName, astNode);
+    }
 
-    public Object getValue(String attributeName, Object astNode) { return null; }
+    /**
+     * Never throws an exception - it is reasonable for a pattern to constrain
+     * via an attribute that not all concrete nodes have.
+     * @return the type in the target program of the given node, or null if this kind
+     * of node has no "target type"
+     */
+    protected Object getTargetType(Object astNode) {
+	return null;
+    }
+
+    /**
+     * Never throws an exception - it is reasonable for a pattern to constrain
+     * via an attribute that not all concrete nodes have.
+     * @return the name in the target program of the given node, or null if this kind
+     * of node has no "name" (e.g. literal expressions)
+     */
+    protected String getName(Object astNode) {
+	return null;
+    }
+
+    protected Object getKind(Object astNode) {
+	final String name= astNode.getClass().getName();
+
+	return name.substring(name.lastIndexOf('.'));
+    }
+
+    /**
+     * Never throws an exception - it is reasonable for a pattern to constrain
+     * via an attribute that not all concrete nodes have.
+     * @return the child with the given role name of the given ast node,
+     * or null if the given kind of ast node has no such attribute
+     */
+    protected Object getChildByRole(String roleName, Object astNode) {
+	Class clazz= astNode.getClass();
+
+        try {
+            while (clazz != null) {
+        	try {
+        	    Field field= clazz.getDeclaredField(roleName);
+
+        	    if (field != null && (field.getModifiers() & Modifier.STATIC) == 0) {
+        		boolean save= field.isAccessible();
+        		field.setAccessible(true);
+        		Object result= field.get(astNode);
+        		field.setAccessible(save);
+        		return result;
+        	    }
+        	} catch (NoSuchFieldException e) {
+        	    // do nothing
+        	}
+        	clazz= clazz.getSuperclass();
+            }
+	} catch (IllegalArgumentException e) {
+	} catch (IllegalAccessException e) {
+	}
+	return null;
+    }
 
     public Object[] getChildren(Object astNode) { return EMPTY; }
 
@@ -70,8 +140,37 @@ public abstract class ASTAdapterBase implements IASTAdapter {
         return null;
     }
 
+    /**
+     * @param pos the 0-based field index
+     */
     public Object getChildAtPosition(int pos, Object astNode) {
-        return getChildren(astNode)[pos];
+        Class clazz= astNode.getClass();
+        Stack<Field[]> fieldStack= new Stack<Field[]>();
+        Stack<Integer> instanceFieldCounts= new Stack<Integer>();
+
+        while (clazz != null) {
+            final Field[] declaredFields= clazz.getDeclaredFields();
+            fieldStack.push(declaredFields);
+            int count= 0;
+            for(int i= 0; i < declaredFields.length; i++) {
+        	if ((declaredFields[i].getModifiers() & Modifier.STATIC) != 0)
+        	    count++;
+            }
+            instanceFieldCounts.push(count);
+            clazz= clazz.getSuperclass();
+        }
+        Field[] fields;
+        int instFieldCount;
+        int idx= pos;
+        for(fields= fieldStack.pop(), instFieldCount= instanceFieldCounts.pop(); idx >= instFieldCount; idx -= instFieldCount, fields= fieldStack.pop(), instFieldCount= instanceFieldCounts.pop())
+            ;
+        for(int i= 0; i < fields.length; i++) {
+            if ((fields[i].getModifiers() & Modifier.STATIC) != 0) {
+        	if (idx-- == 0)
+        	    return fields[i];
+            }
+        }
+        throw new IllegalArgumentException("Invalid child index " + pos + " for type " + astNode.getClass().getName());
     }
 
     public String getChildRoleAtPosition(int pos, String qualNodeType) {
@@ -84,8 +183,9 @@ public abstract class ASTAdapterBase implements IASTAdapter {
 
     public int getPositionOfChildRole(String roleName, String qualNodeType) {
         try {
-            Field[] fields= Class.forName(qualNodeType).getFields();
-    
+            Class clazz= Class.forName(qualNodeType);
+            Field[] fields= clazz.getFields();
+
             for(int i= 0; i < fields.length; i++) {
         	if (fields[i].getName().equals(roleName))
         	    return i;
@@ -94,8 +194,8 @@ public abstract class ASTAdapterBase implements IASTAdapter {
             throw new IllegalArgumentException(e);
         }
     
-        if (roleName.equals(NODE_KIND) || roleName.equals(TARGET_TYPE))
+        if (roleName.equals(KIND) || roleName.equals(TARGET_TYPE))
             throw new IllegalArgumentException(roleName);
-        throw new NoSuchElementException(roleName);
+        return -1;
     }
 }
