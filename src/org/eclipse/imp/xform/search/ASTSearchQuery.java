0aa0191a884fbf1e51f15907206f5eb375093dcc
Philippe Charles <pgc1@rice.edu>
2007-02-13 01:13:17 +0000

    Update to work with new LPG distribution on Sourceforge

--- src/org/eclipse/imp/xform/search/ASTSearchQuery.java
+++ src/org/eclipse/imp/xform/search/ASTSearchQuery.java
@@ -2,7 +2,7 @@ package com.ibm.watson.safari.xform.search;
 
 import java.util.Iterator;
 import java.util.Set;
-import lpg.lpgjavaruntime.IMessageHandler;
+import lpg.javaruntime.IMessageHandler;
 import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.IProject;
 import org.eclipse.core.resources.IResource;
@@ -63,8 +63,14 @@ public class ASTSearchQuery implements ISearchQuery {
     }
 
     class SystemOutMessageHandler implements IMessageHandler {
-	public void handleMessage(int offset, int length, String message) {
-	    System.out.println("[" + offset + ":" + length + "] " + message);
+	public void handleMessage(int errorCode, int [] msgLocation, int[] errorLocation, String filename, String [] errorInfo) {
+            int offset = msgLocation[IMessageHandler.OFFSET_INDEX],
+                length = msgLocation[IMessageHandler.LENGTH_INDEX];
+            String message = "";
+            for (int i = 0; i < errorInfo.length; i++)
+                message += (errorInfo[i] + (i < errorInfo.length - 1 ? " " : ""));
+
+            System.out.println("[" + offset + ":" + length + "] " + message);
 	}
     }
 
